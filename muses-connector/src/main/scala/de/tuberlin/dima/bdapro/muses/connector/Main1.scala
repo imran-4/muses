58d8028cb1fff6a607f9193b2f517d5008d9eb99
Muhammad Imran <mimran.646726@gmail.com>
2018-06-10 20:04:29 +0200

    write to csv. varchar still problemetic

--- muses-connector/src/main/scala/de/tuberlin/dima/bdapro/muses/connector/Main1.scala
+++ muses-connector/src/main/scala/de/tuberlin/dima/bdapro/muses/connector/Main1.scala
@@ -1,12 +1,15 @@
 package de.tuberlin.dima.bdapro.muses.connector
 
-import java.io.File
+import java.io.{BufferedWriter, File, FileWriter}
+import java.nio.ByteBuffer
 import java.sql.{Date, DriverManager, ResultSet}
 import java.{sql, util}
 
 import de.tuberlin.dima.bdapro.muses.connector.arrow.reader.ArrowReader
+import io.netty.buffer.ArrowBuf
 import org.apache.arrow.memory.{AllocationListener, BufferAllocator, RootAllocator}
 import org.apache.arrow.vector._
+import org.apache.arrow.vector.holders.{NullableVarCharHolder, VarCharHolder}
 import org.apache.arrow.vector.ipc.JsonFileWriter
 import org.apache.arrow.vector.types.DateUnit
 import org.apache.arrow.vector.types.pojo._
@@ -24,31 +27,29 @@ object Main1 {
 
       case _  => throw new Exception("Not supported.") // the default, catch-all
     }
+
     return arrowType
   }
 
   var arrowReader :ArrowReader = new ArrowReader
   var arrowWriter :ArrowReader = new ArrowReader
-  def writeData(x: FieldVector, o: Object, count: Int ) : Unit = {
-
+  def writeData(x: FieldVector, o: Object, count: Int, allocator: BufferAllocator ) : Unit = {
     var typ = x.getField.getType
     if (typ.isInstanceOf[ArrowType.Int]) {
       var value = o.asInstanceOf[Int]
       x.asInstanceOf[IntVector].setSafe(count, value)
-
-      println(">>>>>>" , x.asInstanceOf[IntVector].getObject(count))
     }
     else if (typ.isInstanceOf[ArrowType.Date]) {
       var value = o.asInstanceOf[Date]
       x.asInstanceOf[DateDayVector].setSafe(count, value.toLocalDate.getYear)
-      println(">>>>>>" , x.asInstanceOf[DateDayVector].getObject(count))
     } else if (typ.isInstanceOf[ArrowType.Utf8]) {
       var value = o.asInstanceOf[String]
       import java.nio.charset.Charset
-      val utf8Charset = Charset.forName("UTF-8")
+      val utf8Charset:Charset = Charset.forName("UTF-8")
       val arrbuf = value.getBytes(utf8Charset)
-      x.asInstanceOf[VarCharVector].setSafe(count, arrbuf)
-      println(">>>>>>" , x.asInstanceOf[VarCharVector].getObject(count))
+      
+      x.asInstanceOf[VarCharVector].setSafe(count, arrbuf, 0, arrbuf.length)
+      println(">>>>>>########" , x.asInstanceOf[VarCharVector].getObject(count))
     } else {
       throw new Exception("No corresponding vector")
     }
@@ -74,16 +75,13 @@ object Main1 {
     println("Columns")
     var columns: util.ArrayList[Tuple2[String, String]] = new util.ArrayList[Tuple2[String, String]]
     for (i <- 1 to columnsNumber) {
-
       columns.add((rsmd.getColumnName(i),rsmd.getColumnTypeName(i)))
-
       System.out.println(rsmd.getColumnName(i))
     }
 
     def results[T](resultSet: ResultSet)(f: ResultSet => T) = {
       new Iterator[T] {
         def hasNext = resultSet.next()
-
         def next() = f(resultSet)
       }
     }
@@ -95,17 +93,18 @@ object Main1 {
     }
     //........................................................................................
 
+    columns.forEach(x => println(x))
     var fields:util.ArrayList[Field] = new util.ArrayList[Field]()
     columns.forEach(x => {
       fields.add(field(x._1, getArrowType(x._2)))
     })
-
     val sch = new Schema(fields)
-
+    println(sch.toString)
     val allocator: BufferAllocator = new RootAllocator(AllocationListener.NOOP, Long.MaxValue)
     val schemaRoot = VectorSchemaRoot.create(sch, allocator)
     val fieldVectors = schemaRoot.getFieldVectors
 
+    println("***********************")
     var count = -1
     while (it.hasNext) {
       var next = it.next()
@@ -113,47 +112,60 @@ object Main1 {
       fieldVectors.forEach(x => {
         var name = x.getField.getName
         var o1 = next.getObject(name)
-        writeData(x, o1, count)
+        writeData(x, o1, count, allocator)
         x.setValueCount(count)
 
       })
     }
     schemaRoot.setRowCount(count)
+    println("***********************")
     //schemaRoot.close()
     /////////////////////////////////////
     //writing to json file just to see the schema
-    
+
     val writer = new JsonFileWriter(new File("/home/mi/Desktop/empdata.json"), JsonFileWriter.config.pretty(true))
     writer.start(schemaRoot.getSchema, null)
     writer.write(schemaRoot)
     writer.close()
     ////////////////////////////////////
     val fieldVectorsReader = schemaRoot.getFieldVectors
-var count11 = schemaRoot.getRowCount
-    println("####################################: ")
+    var count11 = schemaRoot.getRowCount
+    println("####################################")
     var ccc = 0
+
+    val file = new File("/home/mi/Desktop/out.csv")
+    val bw = new BufferedWriter(new FileWriter(file))
+
+    var fieldsString :String = ""
+    schemaRoot.getSchema.getFields.forEach(x => fieldsString += x.getName + ",")
+    fieldsString += "\r\n"
+    bw.write(fieldsString)
+
     for (ccc <- 0 until count11) {
+      var str:StringBuilder = new StringBuilder
       fieldVectorsReader.forEach(x => {
         var name = x.getField.getName
         var typ1 = x.getField.getType
-
         var dcount = x.getValueCount
-        var ddd = 0
-        //for (ddd <- 0 until dcount) {
-          if (typ1.isInstanceOf[ArrowType.Int]) {
-              var rec = x.asInstanceOf[IntVector].getObject(ccc)
-              println(rec.asInstanceOf[Int])
-            } else if (typ1.isInstanceOf[ArrowType.Date]) {
-              var rec = x.asInstanceOf[DateDayVector].getObject(ccc)
-              println(rec.asInstanceOf[Int])
-            } else if (typ1.isInstanceOf[ArrowType.Utf8]) {
-              var rec = x.asInstanceOf[VarCharVector].getObject(ccc)
-              println(rec.asInstanceOf[Text])
-            }
+        if (typ1.isInstanceOf[ArrowType.Int]) {
+          var rec = x.asInstanceOf[IntVector].getObject(ccc)
+          str.append(rec + ",")
+        } else if (typ1.isInstanceOf[ArrowType.Date]) {
+          var rec = x.asInstanceOf[DateDayVector].getObject(ccc)
+          str.append(rec+ ",")
+        } else if (typ1.isInstanceOf[ArrowType.Utf8]) {
+          println("----------========" + ccc)
+          var rec = x.asInstanceOf[VarCharVector].getObject(0)
+          println(rec)
+          str.append(rec+ ",")
+        }
       })
+      str.append("\r\n")
+      bw.write(str.toString())
     }
+    bw.close()
 
-    println()
+    println("")
   }
 
   import scala.collection.JavaConverters._
