d47ba55e1bee8a634d26ca585b099c47ebc09de2
Muhammad Imran <mimran.646726@gmail.com>
2018-08-12 17:44:18 +0200

    added example command to run muses with yarn

--- bin/run-muses.sh
+++ bin/run-muses.sh
@@ -3,4 +3,8 @@
 echo "Checking prerequisites..."
 
 
+echo "Running Muses..."
+#sample command
+$HADOOP_HOME/bin/yarn jar ../muses-yarn-client/target/muses-yarn-client-1.0.0-SNAPSHOT.jar de.tuberlin.dima.bdapro.muses.yarnclient.Client -jar ../muses-yarn-appmaster/target/muses-yarn-appmaster-1.0.0-SNAPSHOT.jar -number_of_containers 1 -app_jar ../muses-starter/target/muses-starter-1.0.0-SNAPSHOT.jar -conf ../muses-config.json
+
 exit 0
