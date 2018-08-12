47bd15d53270704c3e094cd5b6153f477d46f49f
Muhammad Imran <mimran.646726@gmail.com>
2018-08-12 21:45:34 +0200

    updated command

--- bin/run-muses.sh
+++ bin/run-muses.sh
@@ -4,7 +4,7 @@ echo "Checking prerequisites..."
 
 
 echo "Running Muses..."
-#sample command
-$HADOOP_HOME/bin/yarn jar ../muses-yarn-client/target/muses-yarn-client-1.0.0-SNAPSHOT.jar de.tuberlin.dima.bdapro.muses.yarnclient.Client -jar ../muses-yarn-appmaster/target/muses-yarn-appmaster-1.0.0-SNAPSHOT.jar -number_of_containers 1 -app_jar ../muses-starter/target/muses-starter-1.0.0-SNAPSHOT.jar -conf ../muses-config.json
+#example command
+$HADOOP_HOME/bin/yarn jar muses-yarn-client/target/muses-yarn-client-1.0.0-SNAPSHOT.jar de.tuberlin.dima.bdapro.muses.yarnclient.Client -jar muses-yarn-appmaster/target/muses-yarn-appmaster-1.0.0-SNAPSHOT.jar -number_of_containers 2 -app_jar muses-starter/target/muses-starter-1.0.0-SNAPSHOT.jar -conf muses-config.json -akka_conf muses-akka/target/application.conf
 
 exit 0
